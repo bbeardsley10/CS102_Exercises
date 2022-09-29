6777bbe6a5b2b76113beaa223c5ead26b417be16
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-09-29 14:18:42 -0500

    completed exercise 05

--- Exercise05/mainClass.java
+++ Exercise05/mainClass.java
@@ -1,5 +1,9 @@
 package Exercise05;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * mainClass
  */
@@ -7,6 +11,54 @@ public class mainClass {
 
     public static void main(String[] args) {
         
-        
+
+        /*Example of Linked lists */
+
+        LinkedList myLL = new LinkedList();
+        myLL.add(5);
+        myLL.add(-5);
+        myLL.add(20);
+        myLL.add("Brenton");
+        myLL.add(2.8);
+
+        myLL.remove(); //This removes the first number in the linked list (5)
+        System.out.println(myLL);
+
+        // FIFO - First IN First OUT Data Structure
+        // LIFO - Last IN First OUT Data Structure
+
+
+        /* Implementation of Queue Function */
+
+
+        Queue<String> myQ = new LinkedList();
+        myQ.add("green");
+        myQ.add("yellow");
+        myQ.add("blue");
+        myQ.add("pink");
+        myQ.add("grey");
+
+        System.out.println(myQ);
+        myQ.poll();                    //poll removes the first object in the queue
+        System.out.println(myQ);
+        myQ.poll();
+        System.out.println(myQ);
+
+
+        /* Implmentation of Stack Function */
+
+
+        Stack<String> myStack = new Stack();
+        myStack.push("green");
+        myStack.push("yellow");
+        myStack.push("blue");
+        myStack.push("pink");
+        myStack.push("grey");
+
+        System.out.println(myStack);
+        myStack.pop();              //Removes the last item that was added to the stack
+        System.out.println(myStack);
+        myStack.pop();
+        System.out.println(myStack);
     }
 }
